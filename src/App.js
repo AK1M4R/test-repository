b9235d5308bd1923a1e313804a05b4388af13684
AK1M4R <148259988+AK1M4R@users.noreply.github.com>
2023-12-04 20:29:06 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект Акимова Александра Владимировича";
+  const greeting = "Это тестовый проект Акимов Александр Владимирович";
 
   console.log('This is App ');
   return (
