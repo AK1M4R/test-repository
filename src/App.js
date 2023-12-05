97092c7c05930b744ba55e76c39e2fd8da7c7241
AK1M4R <148259988+AK1M4R@users.noreply.github.com>
2023-12-05 15:14:38 +0300

    Revert "Feat 1"

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   const greeting = "Это тестовый проект Акимова Александра Владимировича";
 
+  console.log('This is App ');
   return (
     <div className="App">
      <header className="App-header">
